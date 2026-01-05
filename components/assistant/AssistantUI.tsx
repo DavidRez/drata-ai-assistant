ad35a6023bb6a7be34d8b5dfaa7a4331f6713b96
David Ramirez <davidraviles@gmail.com>
2026-01-05 11:55:36 -0800

    added CTA, removed redundant state, added aria labels

--- components/assistant/AssistantUI.tsx
+++ components/assistant/AssistantUI.tsx
@@ -1,9 +1,10 @@
 "use client"
 import { useRef, useState, useEffect } from "react"
-import { AssistantConfig, AssistantMessages } from "@/types/assistant"
+import { AssistantConfig, AssistantMessages, AssistantQA } from "@/types/assistant"
 import { setMessages } from "@/utils/messages"
 import { AssistantChatWindow } from "./AssitantChatWindow"
 import { AssistantInput } from "./AssistantInput"
+import Link from "next/link"
 
 type Props = {
   config: AssistantConfig
@@ -17,23 +18,18 @@ const initialMessages: AssistantMessages = [
   }
 ]
 
-// Get answer from QA source
-const getAnswer = (id: string, config: AssistantConfig) => {
-  return config.qaSource.items.find((qa) => qa.id === id)?.answer
-}
-
 export function AssistantUI({ config }: Props) {
   const [messages, setMessagesState] = useState<AssistantMessages>(initialMessages)
-  const [questionClicked, setQuestionClicked] = useState<boolean>(false)
+  const [questionAnswer, setQuestionAnswer] = useState<AssistantQA | null>(null) // The question and answer that the user clicked on
+  const [addCta, setAddCta] = useState(false); // Whether to show the CTA
   const questionClickedRef = useRef<boolean>(false)
 
   useEffect(() => {
-    questionClickedRef.current = questionClicked
-  }, [questionClicked])
+    questionClickedRef.current = !questionAnswer
+  }, [questionAnswer])
 
   return (
-    <section className="bg-white shadow-xs">
-      {/* Header */}
+    <section className="bg-white shadow-xs" role="region" aria-label="Chat assistant">
       <div className="flex flex-col gap-2 mb-4 bg-(--drata-blue) text-white p-4">
         <h2 className="text-xl font-semibold">
           {config.header}
@@ -45,27 +41,40 @@ export function AssistantUI({ config }: Props) {
         messages={messages}
         questions={
           // Suggested Questions
-          !questionClicked && (
+          !questionAnswer && (
             <div className="flex flex-col gap-2 mb-4">
               {config.qaSource.items.map((qa) => (
-                <span
+                <button
                   key={qa.id}
-                  className="transition-all duration-300 w-fit max-w-md px-4 py-2 border rounded-full text-gray-600 cursor-pointer hover:bg-gray-100"
+                  className="transition-all duration-300 w-fit max-w-md px-4 py-2 ml-8 border rounded-full text-gray-600 cursor-pointer hover:bg-gray-100"
                   onClick={() => {
                     setMessagesState(setMessages({ role: "user", content: qa.question }, messages))
                     setTimeout(() => {
                       setMessagesState((currentMessages) => 
-                        setMessages({ role: "assistant", content: getAnswer(qa.id, config) || "" }, currentMessages)
+                        setMessages({ role: "assistant", content: qa.answer || "" }, currentMessages)
                       )
+                      setAddCta(true)
                     }, 2000)
-                    setQuestionClicked(true)
+                    setQuestionAnswer(qa)
                   }}
                 >
                   {qa.question}
-                  </span>
+                  </button>
                 ))}
               </div>
           )
+        }
+        cta={addCta && (
+          <Link
+            href={questionAnswer?.cta?.href || ""}
+            target={questionAnswer?.cta?.openNewTab ? "_blank" : "_self"}
+            rel={questionAnswer?.cta?.openNewTab ? "noopener noreferrer" : undefined}
+            aria-label={questionAnswer?.cta?.openNewTab ? "Open in new tab" : "Open in same tab"}
+            className="transition-all duration-300 rounded-full px-4 py-2 ml-8 border w-fit text-sm font-medium bg-(--cta-blue) text-white hover:bg-white hover:text-(--cta-blue)"
+          >
+            {questionAnswer?.cta?.label}
+          </Link>
+        )
         }
       />
 
@@ -75,6 +84,7 @@ export function AssistantUI({ config }: Props) {
           label={config.body || "Ask a question:"} 
           id="question-input" 
           disabled={true} 
+          readonly={true}
           placeholder="Choose a question above" 
           onClick={() => {}} />
       </div>
