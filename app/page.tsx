54a3aea43d45b04805dbec34de92dda5224f433c
David Ramirez <davidraviles@gmail.com>
2026-01-05 11:45:54 -0800

    modified header content and style

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import { assistantConfig } from "@/constants/data"
 export default function HomePage() {
   return (
     <main className="max-w-5xl mr-auto ml-auto px-6 py-24">
-      <h1 className="text-4xl font-bold mb-12">
-        Homepage Content
+      <h1 className="text-4xl mb-12 text-center">
+      <span className="text-gray-900">Continuous Trust,</span>
+      <br />
+      <span className="text-(--cta-blue)">Powered by AI.</span>
       </h1>
 
       <Assistant config={assistantConfig} />
