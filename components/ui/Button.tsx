b46250811cde7ff44c10bbde46e59962979d2ebb
David Ramirez <davidraviles@gmail.com>
2026-01-05 22:28:47 -0800

    added tertiary style

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -11,10 +11,12 @@ export function Button({
 }: ButtonProps): React.ReactNode {
   return (
     <button
-    className={`transition-all duration-300 rounded px-4 py-2 h-fit ${
+    className={`transition-all duration-300 rounded px-4 py-2 w-fit h-fit cursor-pointer ${
       variant === "primary"
-        ? "bg-(--cta-blue) text-white"
-        : "bg-gray-200 text-gray-900"
+        ? "bg-(--cta-blue) text-white border border-(--cta-blue) hover:bg-white hover:text-(--cta-blue)" :
+        variant === "secondary"
+        ? "bg-gray-200 text-(--drata-blue) border border-(--drata-blue) hover:bg-(--drata-blue) hover:text-white hover:border-white"
+        : "bg-(--drata-blue) text-white border border-white hover:bg-white hover:text-(--drata-blue) hover:border-(--drata-blue)"
     }`}
     onClick={onClick}
   >
